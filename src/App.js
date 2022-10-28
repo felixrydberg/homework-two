557ffb4a6a0346a37fdf9d1017a542f829de3b27
felixrydberg <90419848+felixrydberg@users.noreply.github.com>
2022-10-28 11:20:40 +0200

    file refactoring

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
 import React from "react";
-import Fridge from "./component/fridge";
+import { useState, useEffect } from "react";
+import Inputs from "./component/inputs";
+import Lists from "./component/lists";
 
 const App = () => {
-  return (<>
-    <Fridge></Fridge>
-  </>);
+  const [types, setTypes] = useState([]);
+  const [items, setItems] = useState({});
+
+  const addType = (type) => {
+    if (!types.find((item) => item === type)) setTypes([...types, ...[type]]);
+  };
+
+  const addItem = (data) => {
+    addType(data.category);
+    if (!items[data.category]) items[data.category] = [];
+    if (
+      items[data.category].find(
+        (item) => item.name.toLowerCase() === data.name.toLowerCase()
+      )
+    ) {
+      alert("Item already exists");
+      return;
+    }
+    items[data.category].push(data);
+    setItems(items);
+  };
+
+  return (
+    <>
+      <h1>Fridge</h1>
+      <Inputs callback={addItem}></Inputs>
+      <Lists types={types} items={items}></Lists>
+    </>
+  );
 };
 
 export default App;
